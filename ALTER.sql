2b4e32030b306190800e7abb391570326cf7d1ac
vpserrano <mauelserrano19@gmail.com>
2016-12-16 00:25:52 +0800

    Revised-alter.sql

--- ALTER.sql
+++ ALTER.sql
@@ -1,2 +1,2 @@
-ALTER TABLE `usertypeaccesslevel`
-ADD FOREIGN KEY (`UserType_code`) REFERENCES `usertype` (`code`) ON DELETE CASCADE ON UPDATE CASCADE;
+	ALTER TABLE `usertypeaccesslevel`
+	ADD FOREIGN KEY (`UserType_code`) REFERENCES `usertype` (`code`) ON DELETE CASCADE ON UPDATE CASCADE;
